18d1de38ceb740c8e3f8dcfca6907c7d440fbf81
Naime Medjek <naimemedjek@gmail.com>
2022-12-20 18:43:30 +0100

    food model created

---
+++ FoodWatcherAPI/Models/Food.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FoodWatcherAPI.Models
+{
+    public class Food
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Added { get; set; } = DateTime.Now;
+
+        [DataType(DataType.Date)]
+        public DateTime ExpirationDate { get; set; }
+
+        public Category Category { get; set; }
+    }
+
+    public enum Category
+    {
+        Grain,
+        Vegetable,
+        Protein,
+        Dairy,
+        Fruit,
+        Other
+    }
+
+}
