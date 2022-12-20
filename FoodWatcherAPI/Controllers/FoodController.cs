9e3d4e2d342da9c0e0860d2bc44b74cab1f5ac04
Naime Medjek <naimemedjek@gmail.com>
2022-12-20 20:10:01 +0100

    added create endpoint

--- FoodWatcherAPI/Controllers/FoodController.cs
+++ FoodWatcherAPI/Controllers/FoodController.cs
@@ -22,5 +22,27 @@ namespace FoodWatcherAPI.Controllers
         {
             return await _context.Foods.ToListAsync();
         }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(Food), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var food =  await _context.Foods.FindAsync(id);
+            return food == null ? NotFound() : Ok(food);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<IActionResult> Create(Food food)
+        {
+            await _context.Foods.AddAsync(food);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetById), new { id = food.Id }, food);
+        }
+
+  
+
     }
 }
