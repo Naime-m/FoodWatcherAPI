f2994bc22919f708f2c348a3dad2f9e987f84337
Naime Medjek <naimemedjek@gmail.com>
2022-12-20 18:50:44 +0100

    Added DbContext for food model

---
+++ FoodWatcherAPI/Data/FoodDbContext.cs
@@ -0,0 +1,13 @@
+﻿using FoodWatcherAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodWatcherAPI.Data;
+
+public class FoodDbContext : DbContext
+{
+    public FoodDbContext(DbContextOptions<FoodDbContext> options) : base(options)
+    { }
+
+    public DbSet<Food> Foods { get; set; }
+
+}
